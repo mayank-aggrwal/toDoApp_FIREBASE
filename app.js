28ff3f4a76c9d23893b030edb4cdf99fa75d8b34
ronwon <aggarwal.mayank49@gmail.com>
2019-05-25 13:33:59 +0530

    Display information from firestore

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 
+const toList = document.querySelector('#cafe-list');
+
+function renderList(doc) {
+    li = document.createElement('li');
+    sp1 = document.createElement('span');
+    sp2 = document.createElement('span');
+    li.setAttribute('data-id', doc.id);
+
+    sp1.textContent = doc.data().work;
+    sp2.textContent = doc.data().date;
+
+    li.appendChild(sp1);
+    li.appendChild(sp2);
+    toList.appendChild(li);
+}
+
 db.collection('toDOs').get().then((snapshot) => {
     // console.log(snapshot.docs);
     snapshot.docs.forEach((doc) => {
-        console.log(doc.data().work);
+        renderList(doc);
     });
 });
