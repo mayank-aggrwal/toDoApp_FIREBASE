472a9e19a4b8601513b3d6b0b09cec6e1ac91c44
ronwon <aggarwal.mayank49@gmail.com>
2019-05-25 13:57:49 +0530

    Added functioality to delete an entry

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ function renderList(doc) {
     li = document.createElement('li');
     sp1 = document.createElement('span');
     sp2 = document.createElement('span');
+    cross = document.createElement('div');
     li.setAttribute('data-id', doc.id);
+
+    cross.textContent = 'x';
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('toDOs').doc(id).delete();
+    });
 
     sp1.textContent = doc.data().work;
     sp2.textContent = doc.data().date;
 
+    li.appendChild(cross);
     li.appendChild(sp1);
     li.appendChild(sp2);
     toList.appendChild(li);
